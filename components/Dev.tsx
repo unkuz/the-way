e8a61db0d9276e2edaa0fabd6aca1cf9955a7499
cuzknothz <cuzknothz@gmail.com>
2023-08-11 23:14:43 +0700

    🙄

--- components/Dev.tsx
+++ components/Dev.tsx
@@ -79,7 +79,7 @@ export const Dev = () => {
           className="w-full h-screen  flex justify-center items-center text-[1rem] flex-col gap-[10px]"
         >
           <span className="font-bold tracking-tighter font-[.9rem]">
-            {`This site is under development (${percent}%)`}
+            {`_This site is under development`}
           </span>
           <div className="w-[150px] h-[3px] relative overflow-hidden">
             <div className="h-full  bg-black animate-loading absolute "></div>
