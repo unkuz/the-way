cb278011adacc1540f8eded27ffe1ffc93b7d197
cuzknothz <cuzknothz@gmail.com>
2023-10-13 12:22:19 +0700

    🤣

--- components/IntroAnimation.tsx
+++ components/IntroAnimation.tsx
@@ -14,9 +14,10 @@ export const IntroAnimation = () => {
       delay: 3,
     })
       .to(refZ.current, {
-        scale: 500,
-        duration: 3,
+        scale: 400,
+        duration: 2,
         rotate: '260deg',
+        ease: 'expoScale(0.5,7,none)',
       })
       .set(refX.current, {
         display: 'inline-block',
@@ -25,13 +26,6 @@ export const IntroAnimation = () => {
         transform: 'translateY(0%)',
         scale: 1,
       })
-    //   .to(refX.current!.childNodes, {
-    //     transform: 'translateY(0%)',
-    //     stagger: {
-    //       each: 0.03,
-    //       ease: 'cubic-bezier(0.76,0,0.24,1)',
-    //     },
-    //   })
 
     return () => {
       tl.kill()
@@ -44,7 +38,7 @@ export const IntroAnimation = () => {
         className="uppercase text-[50px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
         ref={refZ}
       >
-        z
+        Z
       </span>
       <div
         ref={refX}
